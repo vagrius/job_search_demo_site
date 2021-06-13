1e4fb99480af906d0e1b7884ea1da44684b2d959
vagrius <vagrius1@bk.ru>
2021-06-13 09:53:04 +0400

    For deploy

--- stepik_jobs/settings.py
+++ stepik_jobs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ilkks&ovij2#)gm9j3cu+v$b-gkfsk$h8_&ai^gb&5lnto)k$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
