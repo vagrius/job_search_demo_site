8f0492d162eeea1551c3a66cc46f489ac39b1d73
vagrius <vagrius1@bk.ru>
2021-06-14 13:56:35 +0400

    Switch WhiteNoise off

--- stepik_jobs/settings.py
+++ stepik_jobs/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'static'
+# STATIC_ROOT = 'static'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -145,3 +147,5 @@ LOGOUT_REDIRECT_URL = '/'
 SESSION_COOKIE_SECURE = False
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
