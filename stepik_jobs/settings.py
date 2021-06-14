f461dc6996327b029981cd30df6346779ddcc081
vagrius <vagrius1@bk.ru>
2021-06-14 17:38:10 +0400

    Little changes

--- stepik_jobs/settings.py
+++ stepik_jobs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ilkks&ovij2#)gm9j3cu+v$b-gkfsk$h8_&ai^gb&5lnto)k$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
